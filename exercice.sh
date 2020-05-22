b5d386029015f9dc19e0b00854e2e769ec912f1d
nonoM33 <renaudlemagicien@gmail.com>
2020-05-23 00:26:24 +0200

    ajout de l'exercice

---
+++ exercice.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+// Coucou mon chat , petit rappel tout ce qui est après // ce sont des commentaires non pris en charge dasn le script ! 
+
+
+// Donc le je vais t'apprendre a chercher une info, je vais donc te faire des petits exercices :)
+
+// ------------------------------------------------------------------
+
+// ---- Exercice 1 ------ (écrit ton code entre le DEBUT et FIN ;) ) 
+
+// Consigne// 
+//j'aimerais que le script me demande mon prenom puis mon age et qu'avec ses deux info il me créer un pseudo. 
+
+// Exemple //
+// prenom: Renaud     age:26    ->   Renaud26 
+
+
+
+// Information pour la recherche //
+// Ca s'appel de la concaténation le fais de fusionner deux variables.
+// Pour rappel tu programmes en Bash là ;) ca vas etre utile pour ta recherches !
+
+
+// DEBUT
+
+
+
+
+
+
+
+
+// FIN
