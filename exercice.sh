8c3b37741ecbefa3b03ccf980b85d145df2c6214
NonoM33 <renaudlemagicien@gmail.com>
2020-05-23 00:31:50 +0200

    Update exercice.sh

--- exercice.sh
+++ exercice.sh
@@ -1,34 +1,20 @@
-#!/bin/bash
+Coucou mon chat , 
 
-// Coucou mon chat , petit rappel tout ce qui est après // ce sont des commentaires non pris en charge dasn le script ! 
+Donc le je vais t'apprendre a chercher une info, je vais donc te faire des petits exercices :)
 
+ Consigne de l'exercice 
 
-// Donc le je vais t'apprendre a chercher une info, je vais donc te faire des petits exercices :)
+j'aimerais que le script me demande mon prenom puis mon age et qu'avec ses deux info il me créer un pseudo.
+Tu éditeras le programme pseudo.sh avec VIM. 
 
-// ------------------------------------------------------------------
+Exemple de l'exercice :
 
-// ---- Exercice 1 ------ (écrit ton code entre le DEBUT et FIN ;) ) 
-
-// Consigne// 
-//j'aimerais que le script me demande mon prenom puis mon age et qu'avec ses deux info il me créer un pseudo. 
-
-// Exemple //
-// prenom: Renaud     age:26    ->   Renaud26 
+votre prenom ? : Renaud
+Votre age ? : 26
+Votre pseudo est : Renaud26 (il me la créer tout seul grace au deux informations qu'on lui a donner !)
 
 
+Information pour la recherche //
+- Ca s'appel de la concaténation le fais de fusionner deux variables.
+- Pour rappel tu programmes en Bash là ;) ca vas etre utile pour ta recherches !
 
-// Information pour la recherche //
-// Ca s'appel de la concaténation le fais de fusionner deux variables.
-// Pour rappel tu programmes en Bash là ;) ca vas etre utile pour ta recherches !
-
-
-// DEBUT
-
-
-
-
-
-
-
-
-// FIN
