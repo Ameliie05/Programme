428530b17d2d1407e59e7f3c46017b4ac7b4450e
Ameliie05 <amelieleroy1@orange.fr>
2020-05-23 15:30:14 +0000

    ajout du menu

--- pseudo.sh
+++ pseudo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-
+concatenationAgePrenom() {
 
 read -p  "quel est ton prenom :" nom
 
@@ -11,5 +11,32 @@ echo "l'age choisi est : $age"
 
 pseudo="${nom} ${age}"
 echo " votre pseudo est : ${pseudo} "
+
+}
+
+choix=("Pseudo aleatoire" "Pseudo a partir prenom + age" "quitter")
+
+select choixUtilisateur in "${choix[@]}"
+do
+   case $choixUtilisateur in
+       "Pseudo aleatoire")
+           echo "vous avez choisi pseudo aleatoire"
+           break
+         ;;
+
+       "Pseudo a partir prenom + age")
+           echo " vous avez choisi pseudo a partir prenom + age"
+           break
+       ;;
+
+       "quitter")
+         break
+       ;;
+
+       *) echo "ERREUR CETTE OPTION N'EXISTE PAS $REPLY"
+          break
+           ;;
+   esac
+ done
 
 
