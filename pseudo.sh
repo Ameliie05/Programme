0c7b01fb6418aae00d21f6e1a6dd9fe29d137dd6
Ameliie05 <amelieleroy1@orange.fr>
2020-05-23 14:18:04 +0000

    ajout de la fonction pseudo

--- pseudo.sh
+++ pseudo.sh
@@ -7,3 +7,9 @@ read -p  "quel est ton prenom :" nom
 echo "le prénom  choisi est : $nom"
 read -p " quel est ton age :" age
 echo "l'age choisi est : $age"
+
+
+pseudo="${nom} ${age}"
+echo " votre pseudo est : ${pseudo} "
+
+
