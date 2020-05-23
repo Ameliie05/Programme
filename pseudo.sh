490a2f859ba30eaae8427365d18809ea858aa4c1
nonoM33 <renaudlemagicien@gmail.com>
2020-05-23 16:55:09 +0000

    ajouter l'option de pseudo aleatoire et l'option de pseudo concatener

--- pseudo.sh
+++ pseudo.sh
@@ -1,31 +1,47 @@
 #!/bin/bash
 
+# fonction pour concatener
+
 concatenationAgePrenom() {
 
 read -p  "quel est ton prenom :" nom
-
 echo "le prénom  choisi est : $nom"
 read -p " quel est ton age :" age
 echo "l'age choisi est : $age"
-
-
 pseudo="${nom} ${age}"
 echo " votre pseudo est : ${pseudo} "
 
 }
 
+# fonction pour le pseudo aleatoire
+
+pseudoAleatoire() {
+
+	read -p "quel est ton prenom :" prenom
+nombre=`echo "$prenom" | wc -m `
+cat /dev/urandom | tr -dc $prenom | fold -w ${1:-$nombre} | head -n 1
+
+}
+
+
+# on créer le tableau (Array) pour les choix 
+
+
 choix=("Pseudo aleatoire" "Pseudo a partir prenom + age" "quitter")
+
+
+# ici on créer le menu 
 
 select choixUtilisateur in "${choix[@]}"
 do
    case $choixUtilisateur in
        "Pseudo aleatoire")
-           echo "vous avez choisi pseudo aleatoire"
+           pseudoAleatoire
            break
          ;;
 
        "Pseudo a partir prenom + age")
-           echo " vous avez choisi pseudo a partir prenom + age"
+           concatenationAgePrenom
            break
        ;;
 
@@ -38,5 +54,3 @@ do
            ;;
    esac
  done
-
-
